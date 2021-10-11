7ca4acdc449cba2d3de67a05b35251a77360eeee
Paul Shields <paul.shields1@btinternet.com>
2021-10-11 16:24:55 +0100

    changed to convert pdf to bmp instead of jpg - file architecture remains the same

--- irfan2.bat
+++ irfan2.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\IrfanView\i_view64.exe" /filelist="F:\PaulShields\PDFs\*.pdf" /convert="F:\PaulShields\jpgs\*.jpg"
+"C:\Program Files\IrfanView\i_view64.exe" /filelist="F:\PaulShields\PDFs\*.pdf" /convert="F:\PaulShields\jpgs\*.bmp"
