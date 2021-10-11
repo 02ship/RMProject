8e00fcd6a47ae9d8050a964894ea497b053d01a7
Paul Shields <paul.shields1@btinternet.com>
2021-10-11 12:24:02 +0100

    refactored calling irfanview

--- irfan2.bat
+++ irfan2.bat
@@ -1,2 +1,1 @@
-cd "C:\Program Files\IrfanView"
-i_view64.exe /filelist="F:\PaulShields\PDFs\*.pdf" /convert="F:\PaulShields\jpgs\*.jpg"
+"C:\Program Files\IrfanView\i_view64.exe" /filelist="F:\PaulShields\PDFs\*.pdf" /convert="F:\PaulShields\jpgs\*.jpg"
