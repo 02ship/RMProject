ccdab772122f6c2d76e2193c9fca3927f41548d4
Paul Shields <paul.shields1@btinternet.com>
2021-10-11 14:34:29 +0100

    invoice generator now generates invoice pdfs based on file name/invoice number - it saves these to a separate folder, preventing recursion

--- invoicegen.bat
+++ invoicegen.bat
@@ -2,4 +2,6 @@
 :: This works using the Invoice_for_PDF.rpt, but not invoice.rpt. I have no idea why.
 :: Next task afterwards will be integrating this with the image conversion script in order to automate invoice generation
 
-"F:\PaulShields\CrystalReportsNinja-master\deployment\CrystalReportsNinja.exe" -F "C:\montana\ReportDocuments\Invoice_for_PDF.rpt" -O "F:\PaulShields\PDFs\testinvoice.pdf" -E pdf -a "HeaderRef:41138662.00" -a "IsCopy:False"
+set arg1=%1
+
+"F:\PaulShields\CrystalReportsNinja-master\deployment\CrystalReportsNinja.exe" -F "C:\montana\ReportDocuments\Invoice_for_PDF.rpt" -O "F:\PaulShields\Invoices\%arg1%.pdf" -E pdf -a "HeaderRef:%arg1%" -a "IsCopy:False"
