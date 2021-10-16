7610a3436daa5842503a12b6f171711cf91eaca1
Paul Shields <paul.shields1@btinternet.com>
2021-10-16 17:20:28 +0100

    invoicegen.bat updated to use powershell to run crystalkiwi

--- invoicegen.bat
+++ invoicegen.bat
@@ -1,6 +1,1 @@
-:: This works using the Invoice_for_PDF.rpt, but not invoice_for_RM.rpt yet due to subreport linked parameters
-
-
-set arg1=%1
-
-"F:\PaulShields\CrystalReportsNinja-master\deployment\CrystalReportsNinja.exe" -F "F:\Montana Reports\Invoice_for_RM.rpt" -O "F:\PaulShields\Invoices\%arg1%.pdf" -E pdf -a "HeaderRef:%arg1%" -a "IsCopy:False" -a "QR_path:F:\PaulShields\jpgs\%arg1%.bmp"
+powershell -Command "& {cd 'C:\Program Files (x86)\CrystalKiwi\Explorer'; Start-Process CrystalKiwiExplorer.exe @F:\PaulShields\commandfile.txt;}"
