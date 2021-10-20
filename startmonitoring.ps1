61f0a88db7819c2612fa0aa6b0c851cff3fe1b4c
Paul Shields <paul.shields1@btinternet.com>
2021-10-20 13:24:39 +0100

    adjusted commandfile creation to output to invoices folder

--- startmonitoring.ps1
+++ startmonitoring.ps1
@@ -13,14 +13,16 @@
                 $logline = "$(Get-Date), $changeType, $path"
                 Add-content "C:\RMProject\PDFs\log.txt" -value $logline
                 Start-Process C:\RMProject\irfan2.bat -NoNewWindow -Wait
-                New-Item C:\RMProject\commandfile.txt
-                Add-Content C:\RMProject\commandfile.txt "report=F:\Montana Reports\Invoice_for_RM.rpt"
-                Add-Content C:\RMProject\commandfile.txt "printer=NPI8E3F7D (HP LaserJet P2055x)"
+                New-Item C:\RMProject\commandfile.txt -ItemType "file"
+                Add-Content C:\RMProject\commandfile.txt "report=C:\RMProject\Invoice_for_RM.rpt"
+                Add-Content C:\RMProject\commandfile.txt "printer=iR C3380"
+                Add-Content C:\RMProject\commandfile.txt ("disk=C:\RMProject\invoices\" + $name.ToString() + ".pdf")
+                Add-Content C:\RMProject\commandfile.txt "format=Acrobat File"
                 Add-Content C:\RMProject\commandfile.txt ("{?HeaderRef}=" + $name.ToString())
                 Add-Content C:\RMProject\commandfile.txt "{?IsCopy}=False"
                 Add-Content C:\RMProject\commandfile.txt ("{?QR_path}=C:\RMProject\jpgs\" + $name.ToString() + ".bmp")
                 Start-Process C:\RMProject\invoicegen.bat -NoNewWindow -Wait
-                Remove-Item C:\RMProject\commandfile.txt
+###                Remove-Item C:\RMProject\commandfile.txt
               }    
 ### DECIDE WHICH EVENTS SHOULD BE WATCHED 
     Register-ObjectEvent $watcher "Created" -Action $action
